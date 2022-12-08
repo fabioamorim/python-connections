06c2aee0ca7c60c82949959d3845e84f4a1b19d8
Fabio Amorim <fabio.amorim@zf.com>
2022-12-08 14:01:31 -0300

    Add exceptions

--- sql/mssql.py
+++ sql/mssql.py
@@ -1,88 +1,99 @@
 import pyodbc 
 
-# Create connection
-def get_connection_sql():
+# Create conn
+def get_conn_sql():
     server = "DESKTOP-S7MI0CA\SQLEXPRESS"
     database = "teste" # db test
     username = "fabioamorim" # user test
     password = "Adm123456789*" # pass test
-    string_connection = "Driver={ODBC Driver 17 for SQL Server};SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+password
-    
-    cnxn = pyodbc.connect(string_connection)
-    
-    return cnxn
+    string_conn = "Driver={ODBC Driver 17 for SQL Server};SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+password
+
+    conn = None 
+
+    try:
+        conn = pyodbc.connect(string_conn)
+    except pyodbc.Erro as err:
+        print(err)
+
+    return conn
 
 # Execute query
-def execute_sql(cnxn, sql, commit=False):
+def execute_sql(conn, sql, commit=False):
 
-    result = cnxn.execute(sql)
+    result = None
 
-    if commit:
-       cnxn.commit() 
+    try:
+        result = conn.execute(sql)
+
+        if commit:
+            conn.commit() 
+
+    except pyodbc.Error as err:
+        print(f"{err}\n{sql}")
 
     return result 
 
-cnxn = get_connection_sql()
+conn = get_conn_sql()
 
 # Create table
 def create_table():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""CREATE TABLE user_login(
             id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
             login VARCHAR(20) NOT NULL
         )"""
 
-    result = execute_sql(cnxn, sql, True)
+    result = execute_sql(conn, sql, True)
 
     return result
 
 # Drop table
 def drop_table():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""DROP TABLE IF EXISTS user_login"""
 
-    result = execute_sql(cnxn, sql, True)
+    result = execute_sql(conn, sql, True)
 
     return result
 
 # Select
 def select_table():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""SELECT * FROM user_login"""
 
-    result = execute_sql(cnxn, sql)
+    result = execute_sql(conn, sql)
 
     return result
 
 # Insert row
 def insert_row():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""INSERT INTO user_login VALUES('foo')"""
 
-    execute_sql(cnxn, sql, True)
+    execute_sql(conn, sql, True)
 
 # Delete row
 def delete_row():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""DELETE FROM user_login WHERE id = 4 """
 
-    execute_sql(cnxn, sql, True)
+    execute_sql(conn, sql, True)
 
 # Update row
 def update_row():
 
-    cnxn = get_connection_sql()
+    conn = get_conn_sql()
 
     sql = f"""UPDATE user_login SET login = 'boo' WHERE id = 5"""
 
-    execute_sql(cnxn, sql, True)
+    execute_sql(conn, sql, True)
